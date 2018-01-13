651caec2137f81dbb999ffe92ccad86a0a706080
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 09:14:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  var plays = []
+  for (var i; i < musicians.length; i++) {
+    plays.push(`${musicians[i]} plays ${instruments[i]}`)
+    return plays
+  }
+}
